323a15d5648badadadbff4d51a25c793c667a5ad
Eduard Rius <erius@leitat.org>
2023-03-17 14:51:53 +0100

    Modify message definitions for camera data

--- free_fleet/src/messages/FleetMessages.h
+++ free_fleet/src/messages/FleetMessages.h
@@ -33,6 +33,13 @@ typedef struct FreeFleetData_RobotMode
   uint32_t mode;
 } FreeFleetData_RobotMode;
 
+extern const dds_topic_descriptor_t FreeFleetData_RobotMode_desc;
+
+#define FreeFleetData_RobotMode__alloc() \
+((FreeFleetData_RobotMode*) dds_alloc (sizeof (FreeFleetData_RobotMode)));
+
+#define FreeFleetData_RobotMode_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_RobotMode_desc, (o))
 
 
 typedef struct FreeFleetData_Location
@@ -45,6 +52,13 @@ typedef struct FreeFleetData_Location
   char * level_name;
 } FreeFleetData_Location;
 
+extern const dds_topic_descriptor_t FreeFleetData_Location_desc;
+
+#define FreeFleetData_Location__alloc() \
+((FreeFleetData_Location*) dds_alloc (sizeof (FreeFleetData_Location)));
+
+#define FreeFleetData_Location_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_Location_desc, (o))
 
 typedef struct FreeFleetData_RobotState_path_seq
 {
@@ -72,6 +86,13 @@ typedef struct FreeFleetData_RobotState
   FreeFleetData_RobotState_path_seq path;
 } FreeFleetData_RobotState;
 
+extern const dds_topic_descriptor_t FreeFleetData_RobotState_desc;
+
+#define FreeFleetData_RobotState__alloc() \
+((FreeFleetData_RobotState*) dds_alloc (sizeof (FreeFleetData_RobotState)));
+
+#define FreeFleetData_RobotState_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_RobotState_desc, (o))
 
 
 typedef struct FreeFleetData_Header
@@ -80,6 +101,41 @@ typedef struct FreeFleetData_Header
   char * frame_id;
 } FreeFleetData_Header;
 
+extern const dds_topic_descriptor_t FreeFleetData_Header_desc;
+
+#define FreeFleetData_Header__alloc() \
+((FreeFleetData_Header*) dds_alloc (sizeof (FreeFleetData_Header)));
+
+#define FreeFleetData_Header_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_Header_desc, (o))
+
+
+typedef struct FreeFleetData_ImagePixel
+{
+  uint32_t pixel;
+} FreeFleetData_ImagePixel;
+
+extern const dds_topic_descriptor_t FreeFleetData_ImagePixel_desc;
+
+#define FreeFleetData_ImagePixel__alloc() \
+((FreeFleetData_ImagePixel*) dds_alloc (sizeof (FreeFleetData_ImagePixel)));
+
+#define FreeFleetData_ImagePixel_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_ImagePixel_desc, (o))
+
+typedef struct FreeFleetData_RobotImage_data_seq
+{
+  uint32_t _maximum;
+  uint32_t _length;
+  FreeFleetData_ImagePixel *_buffer;
+  bool _release;
+} FreeFleetData_RobotImage_data_seq;
+
+#define FreeFleetData_RobotImage_data_seq__alloc() \
+((FreeFleetData_RobotImage_data_seq*) dds_alloc (sizeof (FreeFleetData_RobotImage_data_seq)));
+
+#define FreeFleetData_RobotImage_data_seq_allocbuf(l) \
+((FreeFleetData_ImagePixel *) dds_alloc ((l) * sizeof (FreeFleetData_ImagePixel)))
 
 
 typedef struct FreeFleetData_RobotImage
@@ -92,9 +148,16 @@ typedef struct FreeFleetData_RobotImage
   char * encoding;
   bool is_bigendian;
   uint32_t step;
-  dds_sequence_t data;
+  FreeFleetData_RobotImage_data_seq data;
 } FreeFleetData_RobotImage;
 
+extern const dds_topic_descriptor_t FreeFleetData_RobotImage_desc;
+
+#define FreeFleetData_RobotImage__alloc() \
+((FreeFleetData_RobotImage*) dds_alloc (sizeof (FreeFleetData_RobotImage)));
+
+#define FreeFleetData_RobotImage_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_RobotImage_desc, (o))
 
 
 typedef struct FreeFleetData_ModeParameter
@@ -103,6 +166,13 @@ typedef struct FreeFleetData_ModeParameter
   char * value;
 } FreeFleetData_ModeParameter;
 
+extern const dds_topic_descriptor_t FreeFleetData_ModeParameter_desc;
+
+#define FreeFleetData_ModeParameter__alloc() \
+((FreeFleetData_ModeParameter*) dds_alloc (sizeof (FreeFleetData_ModeParameter)));
+
+#define FreeFleetData_ModeParameter_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_ModeParameter_desc, (o))
 
 typedef struct FreeFleetData_ModeRequest_parameters_seq
 {
@@ -128,6 +198,13 @@ typedef struct FreeFleetData_ModeRequest
   FreeFleetData_ModeRequest_parameters_seq parameters;
 } FreeFleetData_ModeRequest;
 
+extern const dds_topic_descriptor_t FreeFleetData_ModeRequest_desc;
+
+#define FreeFleetData_ModeRequest__alloc() \
+((FreeFleetData_ModeRequest*) dds_alloc (sizeof (FreeFleetData_ModeRequest)));
+
+#define FreeFleetData_ModeRequest_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_ModeRequest_desc, (o))
 
 typedef struct FreeFleetData_PathRequest_path_seq
 {
@@ -152,6 +229,13 @@ typedef struct FreeFleetData_PathRequest
   char * task_id;
 } FreeFleetData_PathRequest;
 
+extern const dds_topic_descriptor_t FreeFleetData_PathRequest_desc;
+
+#define FreeFleetData_PathRequest__alloc() \
+((FreeFleetData_PathRequest*) dds_alloc (sizeof (FreeFleetData_PathRequest)));
+
+#define FreeFleetData_PathRequest_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_PathRequest_desc, (o))
 
 
 typedef struct FreeFleetData_DestinationRequest
@@ -162,6 +246,13 @@ typedef struct FreeFleetData_DestinationRequest
   char * task_id;
 } FreeFleetData_DestinationRequest;
 
+extern const dds_topic_descriptor_t FreeFleetData_DestinationRequest_desc;
+
+#define FreeFleetData_DestinationRequest__alloc() \
+((FreeFleetData_DestinationRequest*) dds_alloc (sizeof (FreeFleetData_DestinationRequest)));
+
+#define FreeFleetData_DestinationRequest_free(d,o) \
+dds_sample_free ((d), &FreeFleetData_DestinationRequest_desc, (o))
 
 #ifdef __cplusplus
 }
